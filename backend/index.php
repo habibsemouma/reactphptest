d82d23f1f61b1029d690b618af119f6b2657e075
Habib Semouma <semoumahabib@gmail.com>
2023-12-05 16:55:29 +0100

    removed comment

--- backend/index.php
+++ backend/index.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');  // Replace * with your allowed domains
+header('Access-Control-Allow-Origin: *'); 
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
